9ab4bc081b1ca8614db0b75fbb3a888aa2004a1e
Med-Amine-Fredj <medamin.fredj@gmail.com>
2022-05-12 18:12:02 +0100

    clean socketIo code

--- index.js
+++ index.js
@@ -47,16 +47,20 @@ devices.Devices.watch().on('change', (data) => {
   }
 });
 
-listeners.connSocket();
-
 PORT = process.env.PORT || 5000;
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function () {
+    console.log('User Disconnected');
+  });
+});
 
-http.listen(5000, () =>
+http.listen(5000, () => {
   winston.info(
     `Server is running on ${process.env.NODE_ENV} mode on port ${PORT}`
       .underline.magenta
-  )
-);
+  );
+});
 
 IOT_PORT = process.env.IOT_PORT || 5001;
 
